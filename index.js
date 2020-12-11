73626a02d7e429e86526f1028fa460438697bf6a
petarPAN <petarburic554@gmail.com>
2020-12-11 20:25:39 +0100

    Add a perimeter(stroke-dasharray) to the circle

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const pauseBtn = document.querySelector('#pause');
 const durationInput = document.querySelector('#duration');
 const circle = document.querySelector('circle');
 
+const perimeter = circle.getAttribute('r') * 2 * Math.PI;
+circle.setAttribute('stroke-dasharray', perimeter);
+
 const timer = new Timer(startBtn, pauseBtn, durationInput, {
     onStart() {
         console.log('start!!')
